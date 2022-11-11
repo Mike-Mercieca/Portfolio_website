4c396f3346d6fbf586a1bef9482381cf0988ec58
Mikegit config --global user.email mikmercieca13@gmail.com <101655537+Mike-Mercieca@users.noreply.github.com>
2022-11-11 11:27:58 +0000

    fix multiple carousel issue

--- index.js
+++ index.js
@@ -26,3 +26,33 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+
+// Functions to control Universally challeneged slideshow
+UnishowSlides(slideIndex);
+
+// Next/previous controls
+function UniplusSlides(n) {
+  UnishowSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function UnicurrentSlide(n) {
+  UnishowSlides(slideIndex = n);
+}
+
+function UnishowSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("UnimySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
